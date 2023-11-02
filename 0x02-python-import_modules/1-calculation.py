41cc7238cd343a386c7d0dc214be9e07a68ee125
Annsrutto <annsrutto567@gmail.com>
2023-11-03 00:25:02 +0300

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,4 +9,4 @@ print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
 print("{} / {} = {}".format(a, b, div(a, b)))
 if __name__ == "__main__":
-    pass # You can add additional code to run here if needed
+    pass  # You can add additional code to run here if needed
