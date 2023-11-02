251706f2fe5623ba316d84a570195ccf5e2af005
Annsrutto <annsrutto567@gmail.com>
2023-11-02 04:42:00 +0300

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
     if i % 2 == 0:
-        char = chr(i - 32)
+        char = chr(i).lower()
     else:
-        char = chr(i)
-    print(char, end='')
+        char = chr(i).upper()
+    print("{}".format(char), end='')
