ed005920e434ff213abe94856ea777916e48f9ef
Annsrutto <annsrutto567@gmail.com>
2023-11-03 00:23:07 +0300

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -9,4 +9,4 @@ print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} * {} = {}".format(a, b, mul(a, b)))
 print("{} / {} = {}".format(a, b, div(a, b)))
 if __name__ == "__main__":
-    pass # Your code to execute when the script is run directly goes here
+    pass # You can add additional code to run here if needed
