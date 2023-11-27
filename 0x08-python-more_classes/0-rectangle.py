946c85769070a4c459e2f3108a07fecff69d4363
Annsrutto <annsrutto567@gmail.com>
2023-11-28 02:06:52 +0300

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 
-class Rectangle:
-    """A class that defines a rectangle.
+"""A class that defines a rectangle"""
 
-    Currently, this class is empty and serves as a placeholder
-    for future developments.
-    """
+
+class Rectangle:
+    """An empty class"""
     pass
