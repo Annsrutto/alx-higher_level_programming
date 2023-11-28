cb495b8cfe4266973f0f8fdf28df5d10d8f6d15d
Annsrutto <annsrutto567@gmail.com>
2023-11-28 04:39:38 +0300

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+
 def print_solution(board):
     solution = []
     for i in range(len(board)):
         solution.append([i, board[i]])
     print(solution)
+
 
 def is_safe(board, row, col):
     for i in range(row):
@@ -14,6 +16,7 @@ def is_safe(board, row, col):
            board[i] + i == col + row:
             return False
     return True
+
 
 def solve_nqueens(board, row):
     if row == len(board):
@@ -26,19 +29,21 @@ def solve_nqueens(board, row):
             solve_nqueens(board, row + 1)
             board[row] = None
 
+
 def nqueens(n):
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
+
     board = [None] * n
     solve_nqueens(board, 0)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         n = int(sys.argv[1])
     except ValueError:
