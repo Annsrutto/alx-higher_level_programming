6e8ed774b6ec92c76f043f65dd37dceea4a703aa
Annsrutto <annsrutto567@gmail.com>
2023-12-07 05:50:59 +0300

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""This module contains a class Student that defines a student."""
+
+
+class Student:
+    """
+    Defines a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiates a Student object with given first name, last name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+        If attrs is a list of strings, only returns attributes contained in this list.
+        """
+        if isinstance(attrs, list) and all(isinstance(item, str) for item in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__
