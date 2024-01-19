58aff33f1f35476af2bd250e774e0e78a0c55861
Annsrutto <annsrutto567@gmail.com>
2024-01-19 04:47:20 +0300

    top-score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table second_table of the database.
+SHOW FROM second_table
+ORDERED BY score ASC, name ASC;
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- lists all records of the table second_table of the database.
+SHOW FROM second_table
+WHERE score >= 10
+ORDERED BY score ASC, name ASC;
