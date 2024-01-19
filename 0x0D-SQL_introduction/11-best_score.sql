41e50e0cb7c80cff6db1110b4de4c692d8f09a72
Annsrutto <annsrutto567@gmail.com>
2024-01-19 04:54:01 +0300

    best-score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,3 @@
 -- lists all records of the table second_table of the database.
-SHOW FROM second_table
-WHERE score >= 10
-ORDERED BY score ASC, name ASC;
+SELECT score, name FROM second_table
+WHERE score >= 10 ORDER BY score DESC;
