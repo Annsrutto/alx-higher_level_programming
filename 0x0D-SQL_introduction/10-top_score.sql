71756bbbf9797c289acb62b8751a450366c6039d
Annsrutto <annsrutto567@gmail.com>
2024-01-19 04:52:44 +0300

    top-score

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,2 @@
 -- lists all records of the table second_table of the database.
-SHOW * FROM second_table
-ORDERED BY score DESC, name DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
