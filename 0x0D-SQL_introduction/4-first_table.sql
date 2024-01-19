2bc749b3d4d444581ca77ced68ada1cfd3511544
Annsrutto <annsrutto567@gmail.com>
2024-01-19 03:10:36 +0300

    first-table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- creates a table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS first_table(
+CREATE TABLE IF NOT EXISTS first_table
     id INT
-    name VARCHAR(256));
+    name VARCHAR(256);
