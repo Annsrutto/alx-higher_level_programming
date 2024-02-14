cf9a02627b33a4a122cddb120e72b71dc8ed394a
Annsrutto <annsrutto567@gmail.com>
2024-02-15 02:19:38 +0300

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,8 @@
 #!/usr/bin/node
-function findSecondBiggest (args) {
-  if (args.length < 3) {
-    return 0; // Return 0 if less than 2 arguments are provided
-  }
-  const numbers = args.map(Number); // Convert arguments to numbers
-  let [biggest, secondBiggest] = numbers[0] > numbers[1] ? [numbers[0], numbers[1]] : [numbers[1], numbers[0]];
+const args = process.argv.slice(2).map(Number).sort((a, b) => a - b);
 
-  for (let i = 2; i < numbers.length; i++) {
-    if (numbers[i] > biggest) {
-      secondBiggest = biggest;
-      biggest = numbers[i];
-    } else if (numbers[i] > secondBiggest) {
-      secondBiggest = numbers[i];
-    }
-  }
-
-  return secondBiggest;
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  console.log(args[args.length - 2]);
 }
-
-const args = process.argv.slice(2);
-console.log(findSecondBiggest(args));
