6d85f527bf6f51a3fb62fd242fc682c0e67b3969
Annsrutto <annsrutto567@gmail.com>
2024-02-19 16:15:13 +0300

    add me maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const callMeMoby = require('./101-call_me_moby').callMeMoby;
-callMeMoby(3, function () {
-  console.log('C is fun');
-});
+function addMeMaybe (number, theFunction) {
+  theFunction(number + 1);
+}
+
+module.exports = { addMeMaybe };
