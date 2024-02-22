be24e65f7ff52c0f6b3c65ce87d59bffeea9d427
Annsrutto <annsrutto567@gmail.com>
2024-02-23 02:42:53 +0300

    Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,37 +1,38 @@
 #!/usr/bin/python3
-"""A script that lists all cities from the database hbtn_0e_4_usa."""
-
+"""
+Lists all cities from the database hbtn_0e_4_usa.
+"""
 import MySQLdb
 import sys
 
 
-def main():
-
-    username, password, database = sys.argv[1],
-    sys.argv[2], sys.argv[3]
-
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-
-    cur = db.cursor()
-
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC
-    """
-
-    cur.execute(query)
-
-    cities = cur.fetchall()
-
-    for city in cities:
-        print(city)
-
-    cur.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT cities.id, cities.name, states.name FROM cities \
+             JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
