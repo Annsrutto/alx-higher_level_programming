9af15339d496911e4620e32bf310a66f153fdd4b
Annsrutto <annsrutto567@gmail.com>
2024-02-23 02:58:14 +0300

    All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,43 +1,35 @@
 #!/usr/bin/python3
-"""A script that takes in the name of a state as an argument
-and lists all cities of that state safe from SQL injections."""
+"""
+Takes in the name of a state as an argument and lists all cities of that state.
+"""
 
 import MySQLdb
 import sys
 
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
 
-def main():
-        if len(sys.argv) == 5:
-        username, password, database, state_name = sys.argv[1], sys.argv[2],
-        sys.argv[3], sys.argv[4]
-    else:
-        print("Usage: ./script.py <mysql username> <mysql password> <database name> <state name>")
-        return
+    state_name = sys.argv[4]
 
-    username, password, database = sys.argv[1],
-    sys.argv[2], sys.argv[3]
+    cursor = db.cursor()
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
+    cursor.execute("SELECT cities.name FROM cities "
+                   "LEFT JOIN states "
+                   "ON states.id = cities.state_id "
+                   "WHERE states.name LIKE BINARY (%s) "
+                   "ORDER BY cities.id ASC",
+                   (state_name,))
 
-    cur = db.cursor()
-
-    query = """
-    SELECT cities.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    ORDER BY cities.id ASC
-    """
-    cur.execute(query, (state_name,))
-
-    rows = cur.fetchall()
-
-    print(", ".join(city[0] for city in cities))
-
-    cur.close()
+    rows = cursor.fetchall()
+    cursor.close()
     db.close()
 
+    str_cities = ", ".join(row[0] for row in rows)
 
-if __name__ == "__main__":
-    main()
+    print(str_cities)
