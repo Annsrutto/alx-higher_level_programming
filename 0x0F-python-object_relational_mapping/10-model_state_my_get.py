35f011d504e8148a94dd9f6eec432d4295b325e0
Annsrutto <annsrutto567@gmail.com>
2024-02-23 04:14:43 +0300

    Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
 """
-Script that prints the first State object from the database hbtn_0e_6_usa
+Script that prints the State object with the name passed as argument
 """
 
 import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
 
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}"
-        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
-    )
+    username, password, database, search_name = sys.argv[1:5]
 
-    Base.metadata.create_all(engine)
+
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
+
+    Base.metadata.bind = engine
 
     Session = sessionmaker(bind=engine)
+
     session = Session()
+
     state = session.query(State).filter(State.name == search_name).first()
 
     if state:
