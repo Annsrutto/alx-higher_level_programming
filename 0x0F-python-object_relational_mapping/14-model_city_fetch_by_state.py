70a399007ee6e4d6dcb95b037faa4795e2584d5b
Annsrutto <annsrutto567@gmail.com>
2024-02-24 02:02:33 +0300

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that prints all City objects from the database hbtn_0e_14_usa
+Script that prints all City objects from the database hbtn_0e_14_usa.
 """
 
 import sys
@@ -10,23 +10,18 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}"
         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
     )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City, State).filter(City.state_id == State.id).order_by(City.id).all()
-    for city, state in cities:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-    
+    result = (session.query(State, City).join
+              (City, State.id == City.state_id).order_by(City.id))
+    for state, city in result:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
     session.close()
