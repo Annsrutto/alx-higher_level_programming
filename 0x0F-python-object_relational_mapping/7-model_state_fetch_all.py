1049d359b2be2b97f77e39d2751a42e7ff0cce5f
Annsrutto <annsrutto567@gmail.com>
2024-02-23 03:45:13 +0300

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,7 +6,6 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
     engine = create_engine(
         f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}'
