2157c5be5245e83635d6d115563a2958cea1b65c
Annsrutto <annsrutto567@gmail.com>
2024-02-23 03:41:00 +0300

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that prints the first State object from the database hbtn_0e_6_usa"""
+"""
+Script that prints the first State object from the database hbtn_0e_6_usa
+"""
 
 import sys
 from model_state import Base, State
@@ -12,18 +14,19 @@ if __name__ == "__main__":
         sys.exit(1)
 
     engine = create_engine(
-        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}'
-        f'@localhost:3306/{sys.argv[3]}',
+        "mysql+mysqldb://{}:{}@localhost:3306/{}"
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
     )
+
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
     first_state = session.query(State).order_by(State.id).first()
-
     if first_state:
-        print(f"{first_state.id}: {first_state.name}")
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
 
