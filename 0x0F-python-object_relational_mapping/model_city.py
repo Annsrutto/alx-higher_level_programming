6ae89c826e090446577bd0709cfd3fc0c3440236
Annsrutto <annsrutto567@gmail.com>
2024-02-24 03:45:32 +0300

    From city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,10 +2,7 @@
 """Defines class City which links to the MySQL table states."""
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
-
-Base = declarative_base()
 
 
 class City(Base):
