172805203f236473f7a5fb52befb1810f048e3fd
Annsrutto <annsrutto567@gmail.com>
2024-03-02 03:27:39 +0300

    requests 3

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     email = {'email': sys.argv[2]}
 
     response = requests.post(url, data=email)
-    print("Your email is:", response.text)
+    print(response.text)
