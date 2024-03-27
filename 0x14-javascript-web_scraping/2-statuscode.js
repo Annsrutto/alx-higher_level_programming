47bdc7d11ec77b4ec385fc597c1f77d164781051
Annsrutto <annsrutto567@gmail.com>
2024-03-27 03:21:52 +0300

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,9 +2,9 @@
 
 const request = require('request');
 
-const GET = process.argv[2];
+const url = process.argv[2];
 
-request(GET, (error, response) => {
+request.get(url, (error, response) => {
   if (!error) {
     console.log('code: ', response.statusCode);
   }
