d14d2289218bddd46bdefda0a808a59fa04bf0d3
Annsrutto <annsrutto567@gmail.com>
2024-03-27 04:29:36 +0300

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const charData = JSON.parse(body).results;
+    console.log(charData.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
